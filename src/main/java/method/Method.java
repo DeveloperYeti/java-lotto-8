87faaeb7b839d004226ab98a713c1b65dc4af928
DeveloperYeti <hoseok9823@gmail.com>
2025-10-29 20:46:16 +0900

    돈 입력받는 메서드 getMoney 작성.

---
+++ src/main/java/method/Method.java
@@ -0,0 +1,23 @@
+package method;
+import lotto.Lotto;
+import java.util.List;
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class Method {
+    // 돈 입력
+    public static int getMoney(){
+        String getMoney = Console.readLine();
+        int money = 0;
+        money = Integer.parseInt(getMoney);
+        return money;
+    }
+
+    // 로또 번호 뽑기
+    public static int getLotto(){
+
+        return 0;
+    }
+
+
+}
