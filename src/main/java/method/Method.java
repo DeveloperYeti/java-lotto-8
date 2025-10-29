b807beb08531cd3165affcc8b6b757ec587a1713
DeveloperYeti <hoseok9823@gmail.com>
2025-10-29 20:54:04 +0900

    입력 받은 돈 1000원당 1장으로 변환하는 메서드 작성.

--- src/main/java/method/Method.java
+++ src/main/java/method/Method.java
@@ -3,11 +3,17 @@ import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
 public class Method {
-
+    // 돈 입력.
     public static int getMoeny(){
         String getMoney = Console.readLine();
         int money = 0;
         money = Integer.parseInt(getMoney);
         return money;
+    }
+    // 입력 받은 돈 1000원당 1장으로 변환
+    public static int devideMoney(){
+        int money = getMoeny();
+        int sum = money%1000 ;
+        return sum;
     }
 }
