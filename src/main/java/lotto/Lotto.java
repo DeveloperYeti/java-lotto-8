68f1af45c76017cbbe21baecb94cdf6228a151c4
DeveloperYeti <hoseok9823@gmail.com>
2025-10-31 13:48:06 +0900

    Validate 부분 검증 로직 추가.

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import java.util.List;
+import java.util.*;
 
 public class Lotto {
     private final List<Integer> numbers;
@@ -9,12 +10,21 @@ public class Lotto {
         validate(numbers);
         this.numbers = numbers;
     }
-
+    // 중복 된 번호가 있을 경우에는 예외처리
     private void validate(List<Integer> numbers) {
         if (numbers.size() != 6) {
             throw new IllegalArgumentException("[ERROR] 로또 번호는 6개여야 합니다.");
         }
+        if (new HashSet<>(numbers).size() != numbers.size()) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호에 중복된 숫자가 있으면 안 됩니다.");
+        }
+        for (Integer n : numbers) {
+            if (n < 1 || n > 45) {
+                throw new IllegalArgumentException("[ERROR] 로또 번호는 1부터 45 사이의 숫자여야 합니다.");
+            }
+        }
     }
+
     
     public List<Integer> getNumbers(){
         return numbers;
